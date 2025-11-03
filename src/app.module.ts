cba8cd08cbfa54fc5469112aa5de117c58cd5478
nasir-sarkar <nasirsarkar537@gmail.com>
2025-11-03 20:26:07 +0600

    Another Module Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PresidentModule } from './club_president/president.module';
+import { MemberModule } from './member/member.module';
 
 @Module({
   imports: [PresidentModule, MemberModule],
